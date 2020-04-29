c8c12e2babbca213d50772a0b9e60966bbdeb475
Ben Mottershead <mottershead.benj@gmail.com>
2020-04-29 18:56:30 +0100

    Revert "ASD"
    This reverts commit a23137f404080bbd5faee68fffe88c194ec53520.

--- OOP - Task 1C/Utils.h
+++ OOP - Task 1C/Utils.h
@@ -7,10 +7,10 @@
 
 class Utils
 {
-public:
-	static std::string getLineFromUser();
-	static char getCharFromUser();
+  public:
+    static std::string getLineFromUser();
+    static char getCharFromUser();
 	static bool isInteger(const std::string&);
 	static std::string formatCredits(const int& amt);
-	// TODO: 2 versions of ToUpper
+    // TODO: 2 versions of ToUpper
 };
